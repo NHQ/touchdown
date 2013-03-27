4edcd6dd5bf693505dc849549c6352b15f28375d
NHQ <jonathon@nationalheadquarters.org>
2013-03-27 13:36:51 -0700

    init & npm publish

---
+++ README.md
@@ -0,0 +1,61 @@
+# touchdown!
+
+This module uses a modified [Touchy.js](https://github.com/jairajs89/Touchy.js) to create normalized events for touch and mouse. The focus is on touch events, and this module is for building user interfaces for that paradigm.
+
+Currently only three events are supported, "touchdown", "vector", and "liftoff", which are analogous to touchstart, touchmove, and touchend, as well as mousedown, mousedrag, and mouseup. A hover event may added, but that is a mouse-only event until the physical computing space gets a little more intimate. Until then, use [mouse-around](https://github.com/NHQ/mousearound)
+
+```
+npm install touchdown
+```
+
+This module is the event core for [UXER](https://github.com/NHQ/uxer), a user interface library with same goals as above.
+
+## usage
+
+```js
+var event = require('touchdown');
+var el = document.getElementById('sweetSpot');
+event.start(el);
+
+// element is now registered for three events, so add listeners:
+el.addEventListener('touchdown', touchdown)
+el.addEventListener('vector', vectorChange)
+el.addEventListener('liftoff', liftoff)
+
+function touchdown(e){
+	var eventDetails = e.detail;
+	var pointOfContact = [ eventDetails.x , eventDetails.y ]
+	// ...
+}
+
+function vectorChange(e){
+	var eventDetails = e.detail;
+	var pointOfContact = [ eventDetails.x , eventDetails.y ]
+	var vector = eventDetails.vector
+	var lastPoint = eventDetails.lastPoint
+	// ...
+}
+
+function liftoff(e){
+	var eventDetails = e.detail;
+	var pointOfContact = [ eventDetails.x , eventDetails.y ]
+	var vector = eventDetails.vector
+	var lastPoint = eventDetails.lastPoint
+	var allPoints = eventDetails.allPoints
+	// ...
+}
+
+```
+
+# api
+
+## .start(element)
+registers the element for events, and starts
+## .register(element)
+registers the element, but events will not be fired until resume(element) is called
+## .pause(element)
+pauses an element, so events will not be called until resumed
+## .resume(element)
+resumes emitting events on that element
+## .end(element)
+end emitting events on that element
---
+++ touchdown.js
@@ -0,0 +1,173 @@
+var touchy = require('./touchy.js')
+,   findPos = require('./findPosition.js')
+,   uuid = require('node-uuid')
+;
+
+module.exports = (function(){
+
+  if(window._touch) return window._touch;
+
+  else return new touch()
+
+}());
+
+function touch(){
+
+  window._touch = this;
+
+  this.elements = [];
+
+  this.touchy = touchy(window, touchtest);
+
+};
+
+touch.prototype.start = touch.prototype.listen = function(el){
+
+    if(!el.touch_id) el.touch_id = ('function' == typeof uuid.v1) ? uuid.v1() : uuid();
+
+    this.elements.push(el);
+
+    el.touch = 1;
+
+};
+
+touch.prototype.register = function(el){
+
+    if(!el.touch_id) el.touch_id = uuid.v1();
+
+    this.elements.push(el);
+
+    el.touch = 0; // needs to be started
+
+};
+
+
+function touchtest(hand, finger){
+	
+	var lastPoint = [], allPoints = [];
+
+  finger.on('start', function(point){
+	
+    var el = search(document.elementFromPoint(point.x, point.y));
+
+    if(el){
+	
+		  lastPoint = [point.x, point.y]
+		
+		  allPoints.push(lastPoint.slice(0))
+
+      this.is = true;
+
+      this.el = el;
+
+      this.event.id = this.id;
+
+      var evt = new CustomEvent('touchdown', { cancelable: true, bubbles: false, detail : point});
+      
+      el.dispatchEvent(evt);
+
+    }
+
+  });
+ 
+  finger.on('move', function(point){
+
+    if(this.is){
+	
+	
+      var evt = new CustomEvent('vector', { cancelable: true, bubbles: false, detail : point});
+
+			evt.detail.vector = [point.x - lastPoint[0], point.y - lastPoint[1]]
+
+			evt.detail.allPoints = allPoints;
+		
+			evt.detail.lastPoint = lastPoint.splice(0)
+						
+			lastPoint = [point.x, point.y]
+		
+		  allPoints.push(lastPoint.slice())
+
+      this.el.dispatchEvent(evt);
+
+    }
+
+  });
+
+  finger.on('end', function(point){
+ 
+    if(this.is){
+
+      var evt = new CustomEvent('liftoff', { cancelable: true, bubbles: false, detail : point});
+
+			evt.detail.vector = [point.x - lastPoint[0], point.y - lastPoint[1]]
+	
+			evt.detail.allPoints = allPoints;
+
+			evt.detail.lastPoint = lastPoint.splice(0)
+
+		  lastPoint = [point.x, point.y]
+
+		  allPoints.push(lastPoint.slice(0))
+
+      this.el.dispatchEvent(evt);
+
+    }
+
+  });
+
+};
+
+function search(el){
+
+  return scan(el)
+
+  function scan(el){
+
+    if(!el) return false;
+  
+    var x = window._touch.elements.reduce(function(val, i){
+
+      if(i.id == el.id && i.touch){
+
+        val = i
+
+      };
+
+      return val
+
+    }, false)
+
+    return x || scan(el.parentElement)
+
+  }
+
+};
+
+
+touch.prototype.pause = function(el){
+
+  el.touch = 0
+
+};
+
+touch.prototype.resume = function(el){
+
+  el.touch = 1
+
+};
+
+touch.prototype.end = function(el){
+
+  delete el.touch
+
+  delete el.touch_id
+
+};
+
+touch.prototype.handleMouse = function(x){
+
+  if(Modernizr) Modernizr.touch = true;
+
+  this.touchy.handleMouse(x);
+
+};
